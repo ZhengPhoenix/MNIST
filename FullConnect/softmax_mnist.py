2486099fbc5cad2552e6b6985745def439956b37
zhenghui <zhenghui@wps.cn>
2019-01-29 17:30:01 +0800

    修改训练脚本

--- FullConnect/softmax_mnist.py
+++ FullConnect/softmax_mnist.py
@@ -18,7 +18,7 @@ print('shape y1: {}'.format(str(y1.shape)))
 print('shape log(y1): {}'.format(str(tf.log(y1).shape)))
 print('shape y_label: {}'.format(str(y_label.shape)))
 
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y1, labels=y_label))
+cross_entropy = tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits(logits=y1, labels=y_label))
 
 train_step = tf.train.GradientDescentOptimizer(0.002).minimize(cross_entropy)
 
@@ -29,12 +29,13 @@ sess.run(init)
 
 # print('w.shape:{}'.format(w.eval(sess)[:, 0].reshape([28, 28, -1]).shape))
 
-for i in range(20000):
-    # if i < 10:
-    #     print('saving step : {}'.format(i))
-    #     w_i = w1.eval(sess)[:, 0].reshape([28, 28])
-    #     np.savetxt('w_' + str(i), w_i)
-    #     print('{} written'.format(i))
+for i in range(2000):
+    if i % 100 == 0:
+        print('saving step : {}'.format(i))
+        for j in range(10):
+            w_i = w1.eval(sess)[:, j].reshape([28, 28])
+            np.savetxt('w_' + str(i) + '_' + str(j), w_i)
+        print('{} written'.format(i))
     batch_xs, batch_ys = mnist.train.next_batch(50)
     sess.run(train_step, feed_dict={x: batch_xs, y_label: batch_ys})
 
--- visualize.py
+++ visualize.py
@@ -1,6 +1,6 @@
+import os
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib as mpl
 from mpl_toolkits.mplot3d import Axes3D
 import argparse
 
@@ -8,18 +8,23 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-i')
 input = parser.parse_args().i
 
-fig = plt.figure()
-ax = Axes3D(fig)
+for file in os.listdir(input):
+    if file.startswith('w_'):
+        fig = plt.figure()
+        ax = Axes3D(fig)
 
-# x and y
-x = np.arange(0, 28)
-y = np.arange(0, 28)
-X, Y = np.meshgrid(x, y)
-xx, yy = X.ravel(), Y.ravel()
-data = np.loadtxt(input)
-z = np.zeros_like(xx + yy)
-data = data.ravel()
-ax.set_zlim(-0.1, 0.1)
-ax.bar3d(xx, yy, z, 1, 1, data, shade=True)
-plt.show()
+        # x and y
+        x = np.arange(0, 28)
+        y = np.arange(0, 28)
+        X, Y = np.meshgrid(x, y)
+        xx, yy = X.ravel(), Y.ravel()
+        data = np.loadtxt(os.path.join(input, file))
+        z = np.zeros_like(xx + yy)
+        data = data.ravel()
+        ax.set_zlim(-0.5, 0.5)
+        ax.bar3d(xx, yy, z, 1, 1, data, shade=True)
+        fname = 'figs/' + file
+        fig.savefig(fname=fname)
+        plt.close(fig)
+
 
